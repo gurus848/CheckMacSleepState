884736534c8f411276ffbf0a43a1b6b452d63f79
GuruSenthil <gurus848@gmail.com>
2016-11-10 17:04:26 +0530

    Initial Commit

---
+++ CheckSleepState/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CheckSleepState
+//
+//  Created by Guru Senthil on 11/10/16.
+//  Copyright © 2016 Guru Senthil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
