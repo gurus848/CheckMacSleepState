4100e44b50a8d422ad25559e969b5762f69e67e3
GuruSenthil <gurus848@gmail.com>
2017-10-06 21:23:37 +0800

    first commit

---
+++ CheckSleepState/ShellInterface.h
@@ -0,0 +1,13 @@
+//
+//  ShellInterface.h
+//  CheckSleepState
+//
+//  Created by Guru Senthil on 11/10/16.
+//  Copyright © 2016 Guru Senthil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShellInterface : NSObject
+
+@end
---
+++ CheckSleepState/ShellInterface.m
@@ -0,0 +1,13 @@
+//
+//  ShellInterface.m
+//  CheckSleepState
+//
+//  Created by Guru Senthil on 11/10/16.
+//  Copyright © 2016 Guru Senthil. All rights reserved.
+//
+
+#import "ShellInterface.h"
+
+@implementation ShellInterface
+
+@end
